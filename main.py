8ac31a9e29a9c1c5055ef7803453ef53a292b733
IceRam <skyfarliv@gmail.com>
2026-02-06 18:46:02 +0300

    06.02.2026

--- main.py
+++ main.py
@@ -8,11 +8,22 @@ UserTime = time.localtime()
 
 def txt(x):
     fd = os.open('./date/txt/logs.txt', os.O_RDWR | os.O_CREAT | os.O_APPEND) #"a+" \|/ a+ dont working i dk
-    os.write(fd, f"{x}\n".encode('utf-8'))
-    os.close(fd)
+    if x == "space":
+        os.write(fd, " ".encode('utf-8'))
+        os.close(fd)
+        return 0
+    if x == "enter":
+        os.write(fd, "\n".encode('utf-8'))
+        os.close(fd)
+        return 0
+    else:
+        os.write(fd, f"{x}".encode('utf-8'))
+        os.close(fd)
+        return 0
 
 def go():
     while(True):
+        print(26)
         if keyboard.read_key():
             txt(keyboard.read_key())
         
@@ -20,7 +31,8 @@ if StartJarvis:
     playsound.playsound("date/sound/JARVIS_start.wav", block=False) #block=False что бы не останавливал программу
     print("hi, i am Jarvis")
     fd = os.open('./date/txt/logs.txt', os.O_RDWR | os.O_CREAT | os.O_APPEND) #"a+" \|/ a+ dont working i dk)
-    os.write(fd, f"User {os.getlogin()}, Год {UserTime[0]}, Месяц {UserTime[1]}, День {UserTime[2]}, Час {UserTime[3]}, Минута {UserTime[4]}\n".encode('utf-8'))
+    os.write(fd, f"\nUser {os.getlogin()}, Год {UserTime[0]}, Месяц {UserTime[1]}, День {UserTime[2]}, Час {UserTime[3]}, Минута {UserTime[4]}\n".encode('utf-8'))
     os.close(fd)
     go()
-print("return 0")
+
+print("return error")
